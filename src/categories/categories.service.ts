4a2255b50396e09ed0bc943e2a2b803a8c451dbd
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-03-16 16:30:19 +0600

    categories.service.ts file has finished and updated

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,25 +1,36 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { categoryDto } from './category-dto/create-category.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Category } from './schema/category-schema';
+import { updateCategoryDto } from './category-dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
-  constructor (@InjectModel(Category.name) private categoryModel: Model<Category>) {};
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<Category>,
+  ) {}
+
+  async findCategory(id: string) {
+    return await this.categoryModel.findById(id);
+  }
 
   async create(data: categoryDto) {
     try {
       return await this.categoryModel.create(data);
     } catch (error) {
       throw new BadRequestException(error.messagw);
-    } 
+    }
   }
 
   async findAll() {
     try {
       let findCategories = await this.categoryModel.find().exec();
-      if(!findCategories.length) return {message: "Categories table empty"};
+      if (!findCategories.length) return { message: 'Categories table empty' };
 
       return findCategories;
     } catch (error) {
@@ -29,15 +40,25 @@ export class CategoriesService {
 
   async findOne(id: string) {
     try {
-      
+      let category = await this.findCategory(id);
+      if (!category) return new NotFoundException('Category not found ❗');
+
+      return { category };
     } catch (error) {
       throw new BadRequestException(error.messagw);
     }
   }
 
-  async update(id: string, data: Partial<categoryDto>) {
+  async update(id: string, data: updateCategoryDto) {
     try {
-      
+      let category = await this.findCategory(id);
+      if (!category) return new NotFoundException('Category not found ❗');
+
+      let newCategory = await this.categoryModel.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+
+      return { newCategory };
     } catch (error) {
       throw new BadRequestException(error.messagw);
     }
@@ -45,7 +66,11 @@ export class CategoriesService {
 
   async remove(id: string) {
     try {
-      
+      let category = await this.findCategory(id);
+      if (!category) return new NotFoundException('Category not found ❗');
+
+      await this.categoryModel.findByIdAndDelete(id);
+      return { message: 'Category deleted successfully ✅' };
     } catch (error) {
       throw new BadRequestException(error.messagw);
     }
