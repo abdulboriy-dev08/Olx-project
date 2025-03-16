df8660ed5dcb38eb8c8eaba0c98d44c3145f0774
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-03-17 00:38:22 +0600

    register-dto.ts file has updated and added a new types, apiproperties

--- src/auth/auth-dto/register-dto.ts
+++ src/auth/auth-dto/register-dto.ts
@@ -1,16 +1,30 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
+
+enum userType {
+  SELLER = 'SELLER',
+  ADMIN = 'ADMIN',
+  CLIENT = 'CLIENT',
+  USER = 'USER',
+}
 
 export class registerDto {
   @IsString()
-  @ApiProperty({ example: 'John Doe' })
+  @ApiProperty({ example: 'Abdulboriy Mahamatjanov' })
   fullName: string;
 
   @IsEmail()
-  @ApiProperty({ example: 'johndoe@example.com' })
+  @ApiProperty({ example: 'abdulborimahammadjanov86@gmail.com' })
   email: string;
 
-  @ApiProperty({ example: '12345' })
+  @ApiProperty({ example: 'StrongPassword123!' })
   password: string;
 
   @IsPhoneNumber()
@@ -21,14 +35,17 @@ export class registerDto {
   avatar: string;
 
   @ApiProperty({ example: 'ADMIN' })
-  userType: string;
+  @IsEnum(userType)
+  userType: userType;
 
   @ApiProperty({ example: '67d25f569d198ce0a684066c' })
   region: string;
 
-  @ApiProperty({ example: 'Magazin' })
+  @ApiPropertyOptional({ example: 'My Awesome Store' })
+  @ValidateIf((o) => o.userType === userType.SELLER)
+  @IsNotEmpty({ message: 'ShopName is required for SELLER' })
   shopName: string;
 
-  @ApiProperty({ example: 'Qatortol street' })
+  @ApiPropertyOptional({ example: 'Tashkent, Uzbekistan' })
   location: string;
 }
