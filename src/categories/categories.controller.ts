dc22577c881c167a90c47ff5b26026f7c29d9641
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-03-16 16:29:33 +0600

    categories.controller.ts file has finished and some updated

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,33 +9,38 @@ import {
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { categoryDto } from './category-dto/create-category.dto';
+import { updateCategoryDto } from './category-dto/update-category.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @ApiOperation({ summary: 'Create a new Categories' })
   @Post()
   create(@Body() CategoryDto: categoryDto) {
     return this.categoriesService.create(CategoryDto);
   }
 
+  @ApiOperation({ summary: 'Get all Categories' })
   @Get()
   findAll() {
     return this.categoriesService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get One Category By ID' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update Categories By ID' })
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() data: Partial<categoryDto>) {
+  update(@Param('id') id: string, @Body() data: updateCategoryDto) {
     return this.categoriesService.update(id, data);
   }
 
+  @ApiOperation({ summary: 'Delete Categories By ID' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(id);
