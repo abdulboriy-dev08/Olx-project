2641986d6a498d36f92ede42245f2e8912f42f41
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-03-16 16:23:56 +0600

    register-dto.ts file has updated and added news

--- src/auth/auth-dto/register-dto.ts
+++ src/auth/auth-dto/register-dto.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
 
 export class registerDto {
+  @IsString()
   @ApiProperty({ example: 'John Doe' })
   fullName: string;
 
+  @IsEmail()
   @ApiProperty({ example: 'johndoe@example.com' })
   email: string;
 
   @ApiProperty({ example: '12345' })
   password: string;
 
+  @IsPhoneNumber()
   @ApiProperty({ example: '+998507525150' })
   phone: string;
 
@@ -24,7 +28,7 @@ export class registerDto {
 
   @ApiProperty({ example: 'Magazin' })
   shopName: string;
-  
+
   @ApiProperty({ example: 'Qatortol street' })
   location: string;
 }
