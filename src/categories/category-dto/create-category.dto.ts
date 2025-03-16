31dd3f4f4d0d7c1560b63b4d39707ce4d480cadc
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-03-16 16:26:57 +0600

    create-category.dto.ts file added and updated

--- src/categories/category-dto/create-category.dto.ts
+++ src/categories/category-dto/create-category.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class categoryDto {
-    name: string;
-    image: string;
+  @IsString()
+  @ApiProperty({ example: 'Cars' })
+  name: string;
+
+  @ApiProperty({ example: '1741847862885.jpeg' })
+  image: string;
 }
